537e3c8a2291ee5d88b68b0d423790405a979310
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-07-27 17:40:54 -0400

    added task

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,31 +1,36 @@
 #!/usr/bin/node
+const argv = process.argv;
+const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
+const urlMovie = `${urlFilm}${argv[2]}/`;
 
 const request = require('request');
-const process = require('process');
 
-function getCharacter (characters, i) {
-  if (characters.length <= i) {
+request(urlMovie, function (error, response, body) {
+  if (error == null) {
+    const fbody = JSON.parse(body);
+    const characters = fbody.characters;
+
+    if (characters && characters.length > 0) {
+      const limit = characters.length;
+      CharRequest(0, characters[0], characters, limit);
+    }
+  } else {
+    console.log(error);
+  }
+});
+
+function CharRequest (idx, url, characters, limit) {
+  if (idx === limit) {
     return;
   }
-
-  request(characters[i], function (_error, response) {
-    console.log(JSON.parse(response.body).name);
-
-    getCharacter(characters, i + 1);
+  request(url, function (error, response, body) {
+    if (!error) {
+      const rbody = JSON.parse(body);
+      console.log(rbody.name);
+      idx++;
+      CharRequest(idx, characters[idx], characters, limit);
+    } else {
+      console.error('error:', error);
+    }
   });
 }
-
-function main () {
-  if (process.argv.length < 3) {
-    return;
-  }
-
-  const film = process.argv[2];
-  request(`https://swapi-api.hbtn.io/api/films/${film}`, function (_error, response) {
-    const characters = JSON.parse(response.body).characters;
-
-    getCharacter(characters, 0);
-  });
-}
-
-main();
