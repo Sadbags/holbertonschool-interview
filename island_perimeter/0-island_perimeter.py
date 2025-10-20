db0f745584e8ff0b03eeea0ceb5545c4f1955936
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-10-19 20:28:30 -0400

    added task

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -5,16 +5,20 @@ that returns the perimeter of the island described in grid:
 
 
 def island_perimeter(grid):
-    """ returns lists  of lists of integers"""
+    """
+    Input: List of Lists
+    Returns: Perimeter of the island
+    """
     count = 0
-    rows = len(grid)
-    cols = len(grid[0]) if row else 0
+    row = len(grid)
+    col = len(grid[0]) if row else 0
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
+
             idx = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
             check = [1 if k[0] in range(row) and k[1] in range(col) else 0
-                    for k in idx]
+                     for k in idx]
 
             if grid[i][j]:
                 count += sum([1 if not r or not grid[k[0]][k[1]] else 0
