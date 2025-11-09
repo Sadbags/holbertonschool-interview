14779b1dd57ad7bd98ecdb1fc6becb3b388b2f28
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-11-09 13:04:36 -0400

    added task

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """ Returns a list of lists representing Pascal's triangle of n """
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        previous_row = triangle[-1]
+        row = [1]
+
+        for j in range(1, i):
+            row.append(previous_row[j - 1] + previous_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
+if __name__ == "__main__":
+    print(pascal_triangle(5))
---
+++ pascal_triangle/README.md
@@ -0,0 +1,8 @@
+# Pascal's Triangle
+
+* Novice
+* By: Alexa Orrico, Software Engineer at Holberton School
+* Weight: 1
+* Your score will be updated as you progress.
+
+Create a function `def pascal_triangle(n):` that returns a list of lists of integers representing the Pascal’s triangle of `n`:
